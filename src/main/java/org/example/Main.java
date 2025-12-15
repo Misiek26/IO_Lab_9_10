1697afba3e6fbc1c5af4db8d359efa0e35130e12
Laptop-HP <micha.ogorek@op.pl>
2025-12-15 10:26:58 +0100

    Add code_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!\n");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
