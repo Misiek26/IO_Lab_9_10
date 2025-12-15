8a4badb4b878bdd211d843cec7416b8b97d5f07d
Laptop-HP <micha.ogorek@op.pl>
2025-12-15 10:33:37 +0100

    Add_test

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,16 +5,11 @@ public class MainTest {
     int a=20;
     int b=20;
 
-    @Test
-    public void firstTest(){
-        if(a>b)
-            System.out.println(a>b);
-    }
 
     @Test
     public void secondTest(){
         int c = a+b;
-        if(c>10){
+        if(c!=10){
             System.out.println(c);
         }
     }
